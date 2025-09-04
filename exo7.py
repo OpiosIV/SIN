a218e4c264f46f34cda513a51d9079ac89db7fa8
OpiosIV <oreacompte974@gmail.com>
2025-09-04 15:36:09 +0200

    Add files via upload

---
+++ exo7.py
@@ -0,0 +1,17 @@
+import math
+a = 5
+b = 16
+c = 3.14 / 2
+d = b / a
+e = b // a
+f = b % a
+g = math.pow(a, 2)
+h = math.sqrt(b)
+i = math.sin(c)
+
+print(f"d={d}")
+print(f"e={e}")
+print(f"f={f}")
+print(f"g={g}")
+print(f"h={h}")
+print(f"i={i}")

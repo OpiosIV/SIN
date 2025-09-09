bf62949779e16f71ee1589f8eae08f9008af2dd4
Evans <oreacompte974@gmail.com>
2025-09-09 16:47:23 +0200

    Add files via upload

---
+++ exo31.py
@@ -0,0 +1,14 @@
+i = 0
+while i < 10:
+    print(f"i vaut: {i}")
+    i = i + 1
+
+print("C'est terminé.")
+
+# le résultat attendu c'est:
+# i vaut: 0
+# i vaut: 1
+# i vaut: 2
+# ...
+# i vaut: 9
+# C'est terminé.

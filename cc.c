eae7cbb5ca971176600953754b1990f44ba2efc4
Evans <oreacompte974@gmail.com>
2025-09-16 17:56:02 +0200

    Create cc.c

---
+++ cc.c
@@ -0,0 +1,20 @@
+int nbmesure = 100;
+long somme = 0;
+
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+        for (int count = 0; count < nbmesure; count++) {
+        int valeur = random(20, 31);
+        somme += valeur;
+      }
+
+      float moyenne = (float)somme / nbmesure;
+
+      Serial.print("Temperature moyenne= ");
+      Serial.print(moyenne);
+      Serial.println(" °C");
+}
